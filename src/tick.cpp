ac6628a1e528f12990104eefb8c71e24f479db99
CodeHz <CodeHz@outlook.com>
2018-07-19 16:10:30 +0800

    Fixed Tick API

--- src/tick.cpp
+++ src/tick.cpp
@@ -7,13 +7,23 @@
 
 #include "base.h"
 
-static std::unordered_multimap<uint16_t, std::function<void(void)>> tickHandles;
+struct FixedFunction {
+  uint16_t chip;
+  std::function<void(void)> fun;
+  FixedFunction(uint16_t chip, std::function<void(void)> fun)
+      : chip(chip)
+      , fun(fun) {}
+  operator uint16_t () { return chip; }
+  void operator()() { fun(); }
+};
+
+static std::unordered_multimap<uint16_t, FixedFunction> tickHandles;
 static std::vector<std::pair<uint16_t, std::function<void(void)>>> timeoutHandles;
 static uint16_t count = 0;
 
 TInstanceHook(void, _ZN5Level4tickEv, Level) {
   for (auto it : tickHandles) {
-    if (count % it.first == 0) {
+    if (count % it.first == it.second) {
       try {
         it.second();
       } catch (std::exception const &e) { Log::error("BASE", "TICK ERROR: %s", e.what()); }
@@ -36,7 +46,10 @@ TInstanceHook(void, _ZN5Level4tickEv, Level) {
 
 extern "C" void mod_init() {
   chaiscript::ModulePtr m(new chaiscript::Module());
-  m->add(chaiscript::fun([](std::function<void(void)> fn, uint16_t cycle) { tickHandles.emplace(cycle, fn); }), "setInterval");
+  m->add(chaiscript::fun([](std::function<void(void)> fn, uint16_t cycle) {
+           tickHandles.emplace(std::make_pair(cycle, FixedFunction{ (uint16_t)(count % cycle), fn }));
+         }),
+         "setInterval");
   m->add(chaiscript::fun([](std::function<void(void)> fn, uint16_t len) { timeoutHandles.push_back(std::make_pair(len, fn)); }), "setTimeout");
   loadModule(m);
 }
