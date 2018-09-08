971e319f1e86c6b9f7f4ec7cc5e4d6ce7ce19d95
CodeHz <codehz@outlook.com>
2018-09-08 20:14:35 +0800

    Fix education
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/mods/education/main.cpp
+++ src/mods/education/main.cpp
@@ -1,32 +1,46 @@
 #include <StaticHook.h>
+#include <string>
 
-TClasslessInstanceHook(bool, _ZNK9LevelData23isEducationEditionLevelEv) { return true; }
+bool asBool(const char *str) {
+  if (str == "0" || str == "false") return false;
+  return true;
+}
 
-TClasslessInstanceHook(bool, _ZNK9LevelData24educationFeaturesEnabledEv) { return true; }
+extern "C" {
+const char *mcpelauncher_property_get(const char *name, const char *def);
+const char *mcpelauncher_property_get_group(const char *group, const char *name, const char *def);
+}
 
-TClasslessInstanceHook(bool, _ZNK17ConnectionRequest9isEduModeEv) { return true; }
+static bool experimental = asBool(mcpelauncher_property_get("experimental", "false"));
+static bool education    = asBool(mcpelauncher_property_get("education", "false"));
+
+TClasslessInstanceHook(bool, _ZNK9LevelData23isEducationEditionLevelEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZNK9LevelData24educationFeaturesEnabledEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZNK17ConnectionRequest9isEduModeEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZNK13LevelSettings23isEducationEditionWorldEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZNK13MinecraftGame9isEduModeEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZThn16_NK13MinecraftGame9isEduModeEv) { return education; }
+
+TClasslessInstanceHook(bool, _ZNK9LevelData30hasExperimentalGameplayEnabledEv) { return experimental; }
+
+TClasslessInstanceHook(bool, _ZNK13LevelSettings31shouldForceExperimentalGameplayEv) { return experimental; }
+
+TClasslessInstanceHook(bool, _ZNK5Level30hasExperimentalGameplayEnabledEv) { return experimental; }
+
+TClasslessInstanceHook(bool, _ZNK12ItemInstance14isExperimentalEv) { return !experimental; }
+
+TClasslessInstanceHook(bool, _ZNK5Actor14isExperimentalEv) { return !experimental; }
 
 TClasslessInstanceHook(bool, _ZNK9LevelData32achievementsWillBeDisabledOnLoadEv) { return false; }
 
 TClasslessInstanceHook(bool, _ZNK9LevelData23hasAchievementsDisabledEv) { return false; }
 
 TClasslessInstanceHook(void, _ZN9LevelData19disableAchievementsEv) {}
-
-TClasslessInstanceHook(bool, _ZNK9LevelData30hasExperimentalGameplayEnabledEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZNK13LevelSettings23isEducationEditionWorldEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZNK13MinecraftGame9isEduModeEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZThn16_NK13MinecraftGame9isEduModeEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZNK13LevelSettings31shouldForceExperimentalGameplayEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZNK5Level30hasExperimentalGameplayEnabledEv) { return true; }
-
-TClasslessInstanceHook(bool, _ZNK12ItemInstance14isExperimentalEv) { return false; }
-
-TClasslessInstanceHook(bool, _ZNK5Actor14isExperimentalEv) { return false; }
 
 struct Item {
   static bool mAllowExperimental;
@@ -37,6 +51,6 @@ struct Enchant {
 };
 
 extern "C" void mod_init() {
-  Item::mAllowExperimental = true;
-  Enchant::mAllowExperimental = true;
+  Item::mAllowExperimental    = experimental;
+  Enchant::mAllowExperimental = experimental;
 }
