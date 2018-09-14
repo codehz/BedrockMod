398b6708a7678690a09fe410afd20a74aecca276
CodeHz <codehz@outlook.com>
2018-09-14 08:34:12 +0800

    enable hidden command
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/mods/edu/main.cpp
+++ src/mods/edu/main.cpp
@@ -13,3 +13,16 @@ hookedu(_ZNK13LevelSettings23isEducationEditionWorldEv);
 hookedu(_ZNK12LevelSummary5isEduEv);
 hookedu(_ZNK9LevelData24educationFeaturesEnabledEv);
 hookedu(_ZNK13LevelSettings24educationFeaturesEnabledEv);
+
+TClasslessInstanceHook(
+    void *, _ZN15CommandRegistry15registerCommandERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEPKc22CommandPermissionLevel11CommandFlagSB_,
+    std::string const &name, const char *prop, char level, char flag1, char flag2) {
+  // printf("%d %d %d %s %s\n", level, flag1, flag2, name.c_str(), prop);
+  return original(this, name, prop, level, flag1 & ~2, flag2);
+}
+
+// TClasslessInstanceHook(void, _ZN12ActorMappingC2ERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEES7_, std::string const &name,
+//                        std::string const &value) {
+//   printf("%s - %s\n", name.c_str(), value.c_str());
+//   original(this, name, value.length() == 0 ? name : value);
+// }
