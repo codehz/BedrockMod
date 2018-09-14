53dbff47ffe0770d5126fdce0831bef20d3239e4
CodeHz <codehz@outlook.com>
2018-09-15 00:43:26 +0800

    Add disable_encryption mods
    Signed-off-by: CodeHz <codehz@outlook.com>

---
+++ src/mods/disable_encryption/main.cpp
@@ -0,0 +1,29 @@
+#include <StaticHook.h>
+#include <string>
+class NetworkIdentifier;
+class ClientToServerHandshakePacket;
+struct ServerNetworkHandler {
+  void handle(NetworkIdentifier const &, ClientToServerHandshakePacket const &);
+};
+TInstanceHook(void, _ZN20ServerNetworkHandler6handleERK17NetworkIdentifierRK11LoginPacket, ServerNetworkHandler, NetworkIdentifier const &netId,
+              void *packet) {
+  original(this, netId, packet);
+  ClientToServerHandshakePacket *whateverhonestly = nullptr;
+  this->handle(netId, *whateverhonestly);
+}
+
+struct NetworkIdentifier {};
+struct Packet {
+  size_t vt;
+};
+
+static auto vt_ServerToClientHandshakePacket = (size_t)dlsym(MinecraftHandle(), "_ZTV29ServerToClientHandshakePacket") + 0x10;
+
+TClasslessInstanceHook(void, _ZN12PacketSender19sendToPrimaryClientERK17NetworkIdentifierRK6Packet, NetworkIdentifier const &id, Packet const &pkt) {
+  if (pkt.vt != vt_ServerToClientHandshakePacket) original(this, id, pkt);
+  else printf("trying sending ServerToClientHandshakePacket!\n");
+}
+
+TClasslessInstanceHook(void, _ZN20EncryptedNetworkPeer16enableEncryptionERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE, std::string const &token) {
+  printf("trying encryption!\n");
+}
