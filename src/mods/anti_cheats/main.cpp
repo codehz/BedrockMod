11c6da1d9d6a49b0ab0b53b948fc43b5f10da300
CodeHz <codehz@outlook.com>
2018-09-21 00:56:52 +0800

    try to disable illegal enchant
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/mods/anti_cheats/main.cpp
+++ src/mods/anti_cheats/main.cpp
@@ -7,6 +7,7 @@ struct EnchantmentInstance {
 };
 
 struct Enchant {
+  enum Type : int{};
   static std::vector<std::unique_ptr<Enchant>> mEnchants;
   int getMaxLevel() const;
 };
@@ -15,4 +16,20 @@ TInstanceHook(void, _ZN19EnchantmentInstance15setEnchantLevelEi, EnchantmentInst
   auto &enchant = Enchant::mEnchants[this->getEnchantType()];
   auto max = enchant->getMaxLevel();
   original(this, max < level ? max : level);
+}
+
+TInstanceHook(int, _ZNK19EnchantmentInstance15getEnchantLevelEv, EnchantmentInstance) {
+  auto level = original(this);
+  auto &enchant = Enchant::mEnchants[this->getEnchantType()];
+  auto max = enchant->getMaxLevel();
+  return max < level ? max : level;
+}
+
+struct ItemInstance;
+
+THook(int, _ZN12EnchantUtils15getEnchantLevelEN7Enchant4TypeERK12ItemInstance, Enchant::Type type, ItemInstance const& item) {
+  auto level = original(type, item);
+  auto &enchant = Enchant::mEnchants[type];
+  auto max = enchant->getMaxLevel();
+  return max < level ? max : level;
 }
