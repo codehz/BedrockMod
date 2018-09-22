6fc06425c590582fd134deb4512ec43cd7fb645f
CodeHz <codehz@outlook.com>
2018-09-22 09:30:38 +0800

    Edu mode: just add edu features, do enter edu mode
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/mods/edu/main.cpp
+++ src/mods/edu/main.cpp
@@ -4,12 +4,14 @@
 #define hookedu(name)                                                                                                                                \
   THook(bool, name, void *) { return true; }
 
+#define hooknotedu(name)                                                                                                                                \
+  THook(bool, name, void *) { return false; }
+
 hookedu(_ZNK15DedicatedServer9isEduModeEv);
 hookedu(_ZNK11AppPlatform9isEduModeEv);
-hookedu(_ZNK17ConnectionRequest9isEduModeEv);
 hookedu(_ZNK5Level5isEduEv);
-hookedu(_ZNK9LevelData23isEducationEditionLevelEv);
-hookedu(_ZNK13LevelSettings23isEducationEditionWorldEv);
+hooknotedu(_ZNK9LevelData23isEducationEditionLevelEv);
+hooknotedu(_ZNK13LevelSettings23isEducationEditionWorldEv);
 hookedu(_ZNK12LevelSummary5isEduEv);
 hookedu(_ZNK9LevelData24educationFeaturesEnabledEv);
 hookedu(_ZNK13LevelSettings24educationFeaturesEnabledEv);
@@ -17,8 +19,8 @@ hookedu(_ZNK13LevelSettings24educationFeaturesEnabledEv);
 TClasslessInstanceHook(
     void *, _ZN15CommandRegistry15registerCommandERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEPKc22CommandPermissionLevel11CommandFlagSB_,
     std::string const &name, const char *prop, char level, char flag1, char flag2) {
-  // printf("%d %d %d %s %s\n", level, flag1, flag2, name.c_str(), prop);
-  return original(this, name, prop, level, flag1 & ~2, flag2);
+  if (flag1 & 2) original(this, name, prop, 1 > level ? 1 : level, flag1 & ~2, flag2);
+  return original(this, name, prop, level, flag1, flag2);
 }
 
 // TClasslessInstanceHook(void, _ZN12ActorMappingC2ERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEES7_, std::string const &name,
