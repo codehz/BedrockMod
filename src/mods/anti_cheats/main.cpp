770ac80b7b5b9f905bb2c7796c04dc27558ada45
CodeHz <codehz@outlook.com>
2018-09-23 16:12:37 +0800

    Remove buggy anti-cheating feature
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/mods/anti_cheats/main.cpp
+++ src/mods/anti_cheats/main.cpp
@@ -68,50 +68,12 @@ struct InventoryTransactionManager {
   ServerPlayer *player;
 };
 
-struct Record {
-  std::uint64_t last;
-  int count;
-};
-
-std::unordered_map<NetworkIdentifier, Record> xmap;
-
-bool testXmap(NetworkIdentifier const &id) {
-  auto &record = xmap[id];
-  auto now     = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-  auto diff    = (double)(now - record.last) / 500;
-  if (record.last == 0) return false;
-  if (record.count == 0 || diff > 1) {
-    record.last  = now;
-    record.count = 1;
-    return false;
-  } else {
-    record.last = now;
-    record.count++;
-  }
-  return record.count > 10;
-}
-
-extern "C" {
-void mod_init() {
-  onPlayerJoined([](ServerPlayer &player) {
-    xmap[player.getClientId()].last =
-        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-  });
-  onPlayerLeft([](ServerPlayer &player) { xmap.erase(player.getClientId()); });
-}
-}
-
 TInstanceHook(void *, _ZN27InventoryTransactionManager9addActionERK15InventoryAction, InventoryTransactionManager, InventoryAction &action) {
   // Log::debug("AC", "(%s:%d) From %s to %s", action.getSource()->toString().c_str(), action.getSlot(), action.getFromItem()->toString().c_str(),
   //            action.getToItem()->toString().c_str());
   if (action.getSource()->getType() == 0) {
     auto c = action.getSource()->getContainerId();
-    if (c == 0) {
-      if (testXmap(player->getClientId())) {
-        Log::debug("AC", "Kicked %s due to fast move inventory", player->getUUID().asString().c_str());
-        ServerCommand::mGame->getNetEventCallback().disconnectClient(player->getClientId(), "Move too fast", false);
-      }
-    } else if (c == 119) {
+    if (c == 119) {
       switch (action.getToItem()->getId()) {
       case 0: break;
       default:
