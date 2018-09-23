1aebe51742099b9429a89f778055e4e1705df4f3
CodeHz <codehz@outlook.com>
2018-09-23 22:30:36 +0800

    test-scripts: tp cooldown
    Signed-off-by: CodeHz <codehz@outlook.com>

--- test-scripts/init.scm
+++ test-scripts/init.scm
@@ -157,22 +157,32 @@
                                       (make-simple-form "Hello" "world" "Done" "Dismiss")
                                       (lambda (x) x))))
 
+(define tp-cooldown-list '())
+(define tp-cooldown 1000)
+
+(define (tp from to)
+        (let [(pos (actor-pos to))
+              (dim (actor-dim to))]
+              (fix-pos pos)
+              (teleport from pos dim)))
+
 (reg-command "tpa"
              "Send a teleport request to other player"
              0
              (list (command-vtable (list (parameter-selector "target" #t))
                                    (checked-player! self
-                                                    (match (command-args)
-                                                          [((target)) (send-form target
-                                                                                 (make-simple-form "Teleport request" (format #f "From ~a" (actor-name self)))
-                                                                               #%(if (json-string->scm %)
-                                                                                     (let* [(pos (actor-pos target))
-                                                                                            (dim (actor-dim target))]
-                                                                                            (fix-pos pos)
-                                                                                            (teleport self pos dim)
-                                                                                            (send-message self "Teleported."))
-                                                                                     (send-message self "Request rejected.")))
-                                                                      (outp-success "Request sent.")]
+                                                    (match (cons (lset<= uuid=? (list (player-uuid self)) tp-cooldown-list) (command-args))
+                                                          [(#t _) (outp-error "Waiting for teleport cooldown")]
+                                                          [(#f (target)) (send-form target
+                                                                                    (make-simple-form "Teleport request" (format #f "From ~a" (actor-name self)))
+                                                                                  #%(if (json-string->scm %)
+                                                                                        (begin (tp self target)
+                                                                                               (send-message self "Teleported."))
+                                                                                        (send-message self "Request rejected.")))
+                                                                         (let [(uuid (player-uuid self))]
+                                                                               (set! tp-cooldown-list (lset-adjoin uuid=? tp-cooldown-list uuid))
+                                                                               (delay-run! tp-cooldown (set! tp-cooldown-list (lset-difference uuid=? tp-cooldown-list (list uuid)))))
+                                                                         (outp-success "Request sent.")]
                                                           [_ (outp-error "Must have 1 player selected")])))))
 
 (reg-command "tpahere"
@@ -180,17 +190,18 @@
              0
              (list (command-vtable (list (parameter-selector "target" #t))
                                    (checked-player! self
-                                                    (match (command-args)
-                                                          [((target)) (send-form target
-                                                                                 (make-simple-form "Teleport request" (format #f "To ~a" (actor-name self)))
-                                                                               #%(if (json-string->scm %)
-                                                                                     (let* [(pos (actor-pos self))
-                                                                                            (dim (actor-dim self))]
-                                                                                            (fix-pos pos)
-                                                                                            (teleport target pos dim)
-                                                                                            (send-message target "Teleported."))
-                                                                                     (send-message self "Request rejected.")))
-                                                                      (outp-success "Request sent.")]
+                                                    (match (cons (lset<= uuid=? (list (player-uuid self)) tp-cooldown-list) (command-args))
+                                                          [(#t _) (outp-error "Waiting for teleport cooldown")]
+                                                          [(#f (target)) (send-form target
+                                                                                    (make-simple-form "Teleport request" (format #f "To ~a" (actor-name self)))
+                                                                                  #%(if (json-string->scm %)
+                                                                                        (begin (tp target self)
+                                                                                               (send-message self "Teleported."))
+                                                                                        (send-message self "Request rejected.")))
+                                                                         (let [(uuid (player-uuid self))]
+                                                                               (set! tp-cooldown-list (lset-adjoin uuid=? tp-cooldown-list uuid))
+                                                                               (delay-run! tp-cooldown (set! tp-cooldown-list (lset-difference uuid=? tp-cooldown-list (list uuid)))))
+                                                                         (outp-success "Request sent.")]
                                                           [_ (outp-error "Must have 1 player selected")])))))
 
 (reg-command "transfer"
