481339a72f676b6252ff30ce27d4cdf05c05953c
CodeHz <codehz@outlook.com>
2018-09-26 23:34:06 +0800

    Fix polymorphic actor
    Signed-off-by: CodeHz <codehz@outlook.com>

--- src/script/base/main.h
+++ src/script/base/main.h
@@ -5,13 +5,16 @@ template <> struct convertible<mce::UUID> : foreign_object_is_convertible<mce::U
 template <> struct convertible<ServerPlayer *> : foreign_object_is_convertible<ServerPlayer *> {};
 template <> struct convertible<ItemActor *> : foreign_object_is_convertible<ItemActor *> {};
 template <> struct convertible<Actor *> : foreign_object_is_convertible<Actor *> {
+  static SCM to_scm(Actor *act) {
+    if (auto player = dynamic_cast<ServerPlayer *>(act); player) { return foreign_object_is_convertible<ServerPlayer *>::to_scm(player); }
+    if (auto itemActor = dynamic_cast<ItemActor *>(act); itemActor) { return foreign_object_is_convertible<ItemActor *>::to_scm(itemActor); }
+    return foreign_object_is_convertible<Actor *>::to_scm(act);
+  }
   static Actor *from_scm(SCM act) {
     if (SCM_IS_A_P(act, foreign_type_convertible<ServerPlayer *>::type())) {
       return (Actor *)foreign_object_is_convertible<ServerPlayer *>::from_scm(act);
     }
-    if (SCM_IS_A_P(act, foreign_type_convertible<ItemActor *>::type())) {
-      return (Actor *)foreign_object_is_convertible<ItemActor *>::from_scm(act);
-    }
+    if (SCM_IS_A_P(act, foreign_type_convertible<ItemActor *>::type())) { return (Actor *)foreign_object_is_convertible<ItemActor *>::from_scm(act); }
     return foreign_object_is_convertible<Actor *>::from_scm(act);
   }
 };
